61e3549dc0deb44b2a476fa5da9ea3133ee2cd99
vitorlc <vitor_cirqueira1@hotmail.com>
2019-12-01 21:29:54 -0300

    Add estudo ponteiros

--- Ponteiros/ponteiros.c
+++ Ponteiros/ponteiros.c
@@ -1,12 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void hora();
+
 int main (){
     int c, *pt;
     c =5;
     pt = &c;
-    printf(">%d\n", c);
-    printf(">%d", *pt);
+
+    printf("===========PONTEIROS===========\n");
+    printf("> Valor da variavel c: %d\n", c);
+    printf("> Endereco da variavel c: %d\n", &c);
+    printf("> Valor do ponteiro pt: %d\n", *pt);
+    printf("> Endereco da variavel c pelo pontiro pt: %d\n", pt);
+    printf("> Endereco da ponteiro pt: %d\n", &pt);
+
+    hora();
 
     return 0;
+}
+
+void hora (){
+
+    printf("===========EXEMPLO COM STRUCT===========");
+    typedef struct horario {
+        int hora;
+        int minuto;
+        int segundo;
+    } Horario;
+    
+    Horario agora, *depois;
+    depois=&agora; //aponto meu ponteiro para o endereço da minha struct agora
+
+    // Atribuo valores a minha struct agora usando meu ponteiro
+    depois->hora = 20;
+    depois->minuto = 20;
+    depois->segundo = 20;
+
+    printf("\n%i:%i:%i", agora.hora, agora.minuto, agora.segundo);
+
 }
